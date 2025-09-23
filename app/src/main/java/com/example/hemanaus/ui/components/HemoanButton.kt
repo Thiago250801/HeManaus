1d839749be47012015def12fd6374b017bbf4137
fabriciocoelho <fabriciocoelho03082000@hotmail.com>
2025-09-22 20:01:18 -0400

    Add Botão Hemoan

--- app/src/main/java/com/example/hemanaus/ui/components/HemoanButton.kt
+++ app/src/main/java/com/example/hemanaus/ui/components/HemoanButton.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -54,4 +55,10 @@ fun HemoamButton(
             fontSize = 16.sp
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HemoamButtonPreview(){
+    HemoamButton(text = "Entrar", onClick = {})
 }
