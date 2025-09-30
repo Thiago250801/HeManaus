c7423193a70a9dbb6e120ef9575a67132bd25792
Thiago Arthur <thlimabsgim@gmail.com>
2025-09-30 00:09:02 -0400

    feat: Enhance RequirementsScreen with checklist and acceptance button

--- app/src/main/java/com/example/hemanaus/ui/screens/RequirementsScreen.kt
+++ app/src/main/java/com/example/hemanaus/ui/screens/RequirementsScreen.kt
@@ -1,22 +1,22 @@
 package com.example.hemanaus.ui.screens
 
-import PhoneMaskTransformation
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CheckCircleOutline
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -26,23 +26,37 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.hemanaus.ui.components.LabeledTextField
-import com.example.hemanaus.ui.components.UserInfo
+import androidx.compose.ui.unit.sp
+import com.hemoam.app.ui.theme.Blue100
+import com.hemoam.app.ui.theme.Blue500
+import com.hemoam.app.ui.theme.Blue700
+import com.hemoam.app.ui.theme.Green600
 import com.hemoam.app.ui.theme.Red600
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RequirementsScreen(
-    onBack : () -> Unit
+    onBack: () -> Unit,
+    onAceppted: () -> Unit
 ) {
+    val requisitos = listOf(
+        "Ter entre 16 e 69 anos",
+        "Pesar no mínimo 50kg",
+        "Estar em bom estado de saúde",
+        "Ter dormido pelo menos 6 horas",
+        "Não ter ingerido bebida alcoólica nas últimas 12 horas",
+        "Estar alimentado (evitar alimentos gordurosos)"
+    )
 
     Scaffold(
         topBar = {
             TopAppBar(
                 navigationIcon = {
                     IconButton(onClick = { onBack() }) {
-                        androidx.compose.material3.Icon(
+                        Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Voltar",
                             tint = Color.White
@@ -88,26 +102,71 @@ fun RequirementsScreen(
                             style = MaterialTheme.typography.titleMedium,
                             color = Color.Black
                         )
+                        requisitos.forEach { requisito ->
+                            Row(
+                                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.CheckCircleOutline,
+                                    contentDescription = null,
+                                    tint = Green600
+                                )
+                                Text(
+                                    text = requisito,
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = Color.Gray
+                                )
+                            }
+                        }
+                    }
+                }
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = CardDefaults.cardColors(
+                        containerColor = Blue100
+                    )
+                ) {
+                    Column (
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(2.dp)){
                         Text(
-                            text = "Para garantir a segurança do doador e do receptor, é importante atender aos seguintes requisitos:",
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = Color.Gray
+                            text = "Importante: ",
+                            style = MaterialTheme.typography.titleMedium,
+                            color = Blue700
                         )
                         Text(
-                            text = "1. Idade: Entre 16 e 69 anos (menores de 18 anos precisam de autorização dos responsáveis).\n" +
-                                    "2. Peso: Mínimo de 50 kg.\n" +
-                                    "3. Saúde: Estar em boas condições de saúde no dia da doação.\n" +
-                                    "4. Intervalo entre doações: Homens podem doar a cada 60 dias, mulheres a cada 90 dias.\n" +
-                                    "5. Alimentação: Evitar alimentos gordurosos nas 3 horas que antecedem a doação.\n" +
-                                    "6. Sono: Ter dormido pelo menos 6 horas nas últimas 24 horas.\n" +
-                                    "7. Hidratação: Estar bem hidratado antes da doação.\n" +
-                                    "8. Documentação: Apresentar um documento oficial com foto (RG, CNH, Carteira de Trabalho, etc.).",
+                            text = "Se você não atende a algum requisito, não se preocupe! Agende para uma data futura quando estiver apto.",
                             style = MaterialTheme.typography.bodyMedium,
-                            color = Color.Gray
+                            color = Blue500
                         )
                     }
                 }
+
+                Button(
+                    onAceppted,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(48.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Red600,
+                        contentColor = Color.White,
+                    ),
+                    shape = MaterialTheme.shapes.large
+                ) {
+                    Text(text = "Atendo aos Requisitos",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 16.sp)
+                }
             }
+
         }
     )
+}
+
+@Preview
+@Composable
+fun RequirementsScreenPreview() {
+    RequirementsScreen(onBack = {}, onAceppted = {})
 }
